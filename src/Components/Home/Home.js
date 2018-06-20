09549094c5964b7aaa5b39f6e23a2247460e4449
jepyhaja <jepyhaja@gmail.com>
2018-06-20 16:10:04 +0300

    Layout for Home.js with BS4 and created new images for links.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,50 +7,64 @@ import './home.css';
 class Home extends Component {
     render() {
       return (
-        <div className="container-fluid">
-          <div className="row pt-5">
-            <div className="col"></div>
-            <div className="col-xs-12 col-md-12 col-lg-8 col-xl-6">
-              <img className="img-fluid" src="/Images/Home/cst-blogo.png" alt="CST Charlie's Surfing Team"/>
+        <div>
+          <div className="container-fluid">
+            <div className="row pt-4">
+              <div className="col"></div>
+              <div className="col-xs-12 col-md-12 col-lg-8 col-xl-6 pb-5">
+                <img className="img-fluid" src="/Images/Home/cst-blogo.png" alt="CST Charlie's Surfing Team"/>
+              </div>
+              <div className="col"></div>
             </div>
-            <div className="col"></div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12 col-md-4 col-lg-3 col-xl-2">
-              <img className="img-fluid mobile-hide" src="/Images/Home/adrian.png" alt="Adrian"/>
-            </div>
-            <div className="col-xs-12 col-md-8 col-lg-7 col-xl-8 pt-5">
-              <p className="paragraph">
-              “‘After lifen' käsite saa uuden merkityksen 15- vuotiaan Howien paiskautuessa itsemurhansa yhteydessä vuosikymmeniä taaksepäin. 
-              Robottisodan uhka väreilee ilmassa, mutta onneksi Team Barracuda - erikoisoperaatioon valikoituu vain kymmenen parasta miestä - 
-              tai naista tai määrittelemätöntä. Vuorien takana liskokansa heräilee satojen vuosien hiljaiselon jälkeen. 
-              Myös banaaniviidakon siimeksessä unohdettu villiheimo nostaa päätään ensimmäisen kerran sitten surullisenkuuluisan tohtori Deadrockin tutkimusretken. 
-              Robottien kanssa yhteen ottaneen Lynxtonien aatelissuvun viimeiseksi jäänyt Adrian teroittaa kyntensä ja on valmiina kostoon - 
-              kunhan armeijan hernekeittoa saa myös gluteiinittomana.”
-              </p>
-              <li className="list-inline-item pt-2 pb-3"><Link to="/comic"><img className="img-fluid" src="/Images/Home/linkki-sarjis-vaaka.png" alt="Linkki sarjakuvaan"/></Link></li>
-            </div>
-            <div className="col">
-            
-            </div>
-          </div>
-          
-          <div className="row pt-2">
-            <div className="col-12">
-            <h2>Tutustu sarjakuvan maailmaan ja hahmoihin</h2>
+            <div className="row pb-5">
+              <div className="col order-md-4"></div>
+              <div className="col-4 col-md-3 col-lg-2 pb-1 pt-2 order-md-5">
+                <Link to="/comic"><img className="img-fluid saturate" src="/Images/Links/linkki-sarjis-vaaka.png" alt="Linkki sarjakuvaan"/></Link>
+              </div>
+              <div className="col order-md-6"></div>
+              <div className="col-xs-12 col-md-3 col-lg-2 col-xl-2 order-md-1">
+                <img className="img-fluid mobile-hide absolute" src="/Images/Home/adrian.png" alt="Adrian"/>
+              </div>
+              <div className="col-xs-12 col-md-8 col-lg-8 col-xl-8 order-md-2">
+                <p className="paragraph">
+                “‘After lifen' käsite saa uuden merkityksen 15- vuotiaan Howien paiskautuessa itsemurhansa yhteydessä vuosikymmeniä taaksepäin. 
+                Robottisodan uhka väreilee ilmassa, mutta onneksi Team Barracuda - erikoisoperaatioon valikoituu vain kymmenen parasta miestä - 
+                tai naista tai määrittelemätöntä. Vuorien takana liskokansa heräilee satojen vuosien hiljaiselon jälkeen. 
+                Myös banaaniviidakon siimeksessä unohdettu villiheimo nostaa päätään ensimmäisen kerran sitten surullisenkuuluisan tohtori Deadrockin tutkimusretken. 
+                Robottien kanssa yhteen ottaneen Lynxtonien aatelissuvun viimeiseksi jäänyt Adrian teroittaa kyntensä ja on valmiina kostoon - 
+                kunhan armeijan hernekeittoa saa myös gluteiinittomana.”
+                </p>
+              </div>
+              <div className="col-xs-12 col-md-1 col-lg-2 col-xl-2 order-md-3"></div>
             </div>
           </div>
-          <div className="row pt-2">
-            <div className="col-4">
-            <h3>Maailma</h3>
+          <div className="container-fluid bg-color-darkergray pt-5">
+            <div className="row pb-4">
+              <div className="col-12">
+                <h2>Katsaus sarjakuvan maailmaan</h2>
+              </div>
             </div>
-            <div className="col-4">
-            <h3>Hahmot</h3>
+            <div className="row pt-2">
+            <div className="col order-md-2"></div>
+              <div className="col-4 col-md-3 col-lg-2 order-md-3">
+                <Link to="/world/globinet"><img className="img-fluid saturate" src="/Images/Links/linkki-globinet-vaaka.png" alt="Globinet linkki"/></Link>
+              </div>
+              <div className="col col-lg-1 order-md-4"></div>  
+              <div className="col-4 col-md-3 col-lg-2 order-md-5">
+                <Link to="/world/society"><img className="img-fluid saturate" src="/Images/Links/linkki-yhteiskunta-vaaka.png" alt="Yhteiskunta linkki"/></Link>
+              </div>
+              <div className="col order-md-6"></div>
+              <div className="col-12 order-md-1">
+                <p className="paragraph">Tähän joku johdatteleva teksti yhteiskuntaan ja maailmaan liittyen</p>
+              </div>
             </div>
-            <div className="col-4">
-            <h3>Tekijät</h3>
+              <div className="col-xs-12 col-md-12 col-lg-4 col-xl-4">
+              <h3>Hahmot</h3>
+              </div>
+              <div className="col-xs-12 col-md-12 col-lg-4 col-xl-4">
+              <h3>Tekijät</h3>
+              </div>
             </div>
-          </div>
         </div>
       );
     }
