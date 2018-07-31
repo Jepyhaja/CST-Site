4bf5c81243d5f65bf9cd2eb2342d4c02028facd6
Jepyhaja <jepyhaja@gmail.com>
2018-07-31 15:28:40 +0300

    JSONified the textfiles and modified the character pages to load character specific data from the json. Also had to modify the way the TextAndImg component is loaded to make the pages work properly.

--- src/Components/Characters/Characters.js
+++ src/Components/Characters/Characters.js
@@ -2,9 +2,26 @@ import React, {Component} from 'react';
 import CharsBanner from './CharsBanner';
 import TextAndImg from '../TextAndImage/TextAndImg';
 import './characters.css';
+const Data = require('./CharTexts.json');
 
+const CharacterDetails = props =>{
+  if(!props.name){
+    return null;
+  }
+  const array = props.getTxt(props.name)
+  return <TextAndImg type={props.type} textStyle={props.textStyle}   imageStyle={props.imageStyle}  name={props.name} char={array}/>
+}
 
 class Characters extends Component {
+  getTxt = (name) => {
+    var CharArray = Data.Chars;
+    for(let i=0;i<CharArray.length; i++){
+      if(CharArray[i].id === name){
+        return CharArray[i]  
+      }
+    }
+  }
+  
   componentDidMount(){
     window.scrollTo(0,0);
   }
@@ -12,18 +29,18 @@ class Characters extends Component {
       return (
           <div className="container-fluid">
             <CharsBanner header={this.props.type} name={this.props.chars}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleRight} image={this.props.StyleLeft} name={this.props.chars[0]}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleLeft} image={this.props.StyleRight} name={this.props.chars[1]}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleRight} image={this.props.StyleLeft} name={this.props.chars[2]}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleLeft} image={this.props.StyleRight} name={this.props.chars[3]}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleRight} image={this.props.StyleLeft} name={this.props.chars[4]}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleLeft} image={this.props.StyleRight} name={this.props.chars[5]}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleRight} image={this.props.StyleLeft} name={this.props.chars[6]}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleLeft} image={this.props.StyleRight} name={this.props.chars[7]}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleRight} image={this.props.StyleLeft} name={this.props.chars[8]}/>
-            <TextAndImg type={this.props.type} text={this.props.StyleLeft} image={this.props.StyleRight} name={this.props.chars[9]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleRight}  imageStyle={this.props.StyleLeft}   name={this.props.chars[0]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleLeft}   imageStyle={this.props.StyleRight}  name={this.props.chars[1]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleRight}  imageStyle={this.props.StyleLeft}   name={this.props.chars[2]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleLeft}   imageStyle={this.props.StyleRight}  name={this.props.chars[3]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleRight}  imageStyle={this.props.StyleLeft}   name={this.props.chars[4]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleLeft}   imageStyle={this.props.StyleRight}  name={this.props.chars[5]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleRight}  imageStyle={this.props.StyleLeft}   name={this.props.chars[6]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleLeft}   imageStyle={this.props.StyleRight}  name={this.props.chars[7]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleRight}  imageStyle={this.props.StyleLeft}   name={this.props.chars[8]}/>
+            <CharacterDetails getTxt={this.getTxt} type={this.props.type} textStyle={this.props.StyleLeft}   imageStyle={this.props.StyleRight}  name={this.props.chars[9]}/>
           </div>
-      );
+      );  
     }
 }
   
--- src/Components/TextAndImage/TextAndImg.js
+++ src/Components/TextAndImage/TextAndImg.js
@@ -2,26 +2,7 @@ import React, {Component} from 'react';
 
 class TextToRight extends Component {
     state = {
-        p1: '',
-        p2: '',
-        p3: ''
-    }
-    componentWillMount=()=>{
-        this.getTxt(this.props.name);
-    }
-    getTxt = async (name) => {
-        const file = await fetch('/TextFiles/Chars/Main/'+name+'.txt')
-        const text = await file.text();
-        if(text[0] === '<'){
-            return null;
-        }else{
-            const splitted = text.split('/')
-            this.setState({
-                p1:splitted[0],
-                p2:splitted[1],
-                p3:splitted[2]
-            })
-        }
+        p1:''
     }
     scrollToTop = () =>{
         var doc = document.documentElement;
@@ -31,6 +12,14 @@ class TextToRight extends Component {
             setTimeout(this.scrollToTop, 1)
         }
     }
+    componentDidMount(){
+        this.setState({
+            p1: this.props.char.Text.p1,
+            p2: this.props.char.Text.p2,
+            p3: this.props.char.Text.p3,
+            p4: this.props.char.Text.p4
+        });
+    }
     render() {
         if(!this.props.name){
             return null;
@@ -39,8 +28,8 @@ class TextToRight extends Component {
         <div>    
             <div id={this.props.name} className="row pt-5">
             <div className="col order-1"></div>
-                <div className={"col-md-12 col-lg-6 "+ this.props.image}><img className="img-fluid charcard" src={"/Images/"+this.props.type+"/"+this.props.name+".png"} alt={this.props.name}/></div>
-                <div className={"col-md-12 col-lg-5 col-xl-4 "+ this.props.text}><p className="chardesc paragraph">{this.state.p1}</p><p className="paragraph">{this.state.p2}</p><p className="paragraph">{this.state.p3}</p></div>
+                <div className={"col-md-12 col-lg-6 "+ this.props.imageStyle}><img className="img-fluid charcard" src={"/Images/"+this.props.type+"/"+this.props.name+".png"} alt={this.props.name}/></div>
+                <div className={"col-md-12 col-lg-5 col-xl-4 "+ this.props.textStyle}><p className="chardesc paragraph">{this.state.p1}</p><p className="paragraph">{this.state.p2}</p><p className="paragraph">{this.state.p3}</p><p className="paragraph">{this.state.p4}</p></div>
             <div className="col order-4"></div>
             </div>
             <button className="btn btn-secondary" onClick={this.scrollToTop}>Takaisin ylös</button>
